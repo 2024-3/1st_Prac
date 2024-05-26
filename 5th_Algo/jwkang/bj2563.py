86b3d416f42f0ba77f329589a47c07e898428c14
jwkang1815@gmail.com <mosfet830@gmail.com>
2024-05-27 04:02:48 +0900

    jwkang-Algo_5

--- 5th_Algo/jwkang/bj2563.py
+++ 5th_Algo/jwkang/bj2563.py
@@ -1,19 +1,19 @@
 import sys
 
 def area_calc(num):
-    area = [[0]*100 for _ in range(100)]
-    total = 0
+    base = [[0]*100 for _ in range(100)]
+    area = 0
 
     for _ in range(num):
         x, y = map(int, sys.stdin.readline().split())
         for i in range(x, x+10):
             for j in range(y, y+10):
-                area[i][j] = 1
+                base[i][j] = 1
  
     for i in range(100):
-        total += area[i].count(1)
+        area += base[i].count(1)
 
-    print(total)
+    print(area)
 
 
 if __name__ == '__main__':
